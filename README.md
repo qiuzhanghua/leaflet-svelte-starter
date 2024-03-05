e7bdfdc1befecd40d509e75a3a1acaf750998311
qiuzhanghua <qiuzhanghua@icloud.com>
2024-03-06 07:45:44 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Svelte + TS + Vite
+
+## Features
+- leaflet map
+- geojson layer
---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import HelloLeaflet from "./lib/HelloLeaflet.svelte";
+</script>
+
+<main>
+    <HelloLeaflet />
+</main>
