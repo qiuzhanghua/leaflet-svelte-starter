b10f29346740b4eb28d2d5db4749643d2cc11556
qiuzhanghua <qiuzhanghua@icloud.com>
2024-03-06 19:40:33 +0800

    feat: locate

--- src/lib/HelloLeaflet.svelte
+++ src/lib/HelloLeaflet.svelte
@@ -6,9 +6,11 @@
   import markerIconUrl from '../../node_modules/leaflet/dist/images/marker-icon.png';
   import markerIconRetinaUrl from '../../node_modules/leaflet/dist/images/marker-icon-2x.png';
   import markerShadowUrl from '../../node_modules/leaflet/dist/images/marker-shadow.png';
-  import { expoIn } from 'svelte/easing';
+
 
   let map: L.Map | L.LayerGroup<any> | null = null;
+  let markerCurrent: L.Marker | null = null;
+
   onMount(() => {
     map = L.map('map').setView([39.90217, 116.3912757], 8);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -24,11 +26,11 @@
     L.Icon.Default.prototype.options.shadowUrl = markerShadowUrl;
     L.Icon.Default.imagePath = ''; // necessary to avoid Leaflet adds some prefix to image path.
 
-    L.marker([39.90217, 116.3912757])
-      .addTo(map)
-      .bindPopup('This is Beijing.')
-      .openPopup();
-
+    // L.marker([39.90217, 116.3912757])
+    //   .addTo(map)
+    //   .bindPopup('This is Beijing.')
+    //   .openPopup();
+    map.locate();
     let someFeatures = [
       {
         type: 'Feature',
@@ -70,6 +72,26 @@
     map.on('contextmenu', function (e) {
       L.marker(e.latlng)
         .addTo(map).bindPopup(e.latlng.toString());
+    });
+
+    map.on('keypress', function (e) {
+      if (e.originalEvent.key == 'l') {
+        console.log(e);
+        map.locate();
+      }
+    });
+
+    map.on('locationfound', function (e) {
+      var radius = e.accuracy / 2;
+      if (markerCurrent) {
+        markerCurrent.remove();
+      }
+      markerCurrent = L.marker(e.latlng)
+        .addTo(map)
+        .bindPopup('You are within ' + radius + ' meters from this point')
+        .openPopup();
+      L.circle(e.latlng, radius).addTo(map);
+      // map.setView(e.latlng, 16);
     });
 
 
